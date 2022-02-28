b73d62372bf675618c4b3457a4056bc0523d79a2
Kebechet <noreply+kebechet@redhat.com>
2022-02-28 22:07:18 +0000

    Release of version 0.6.0

--- thoth/prescriptions_refresh/__init__.py
+++ thoth/prescriptions_refresh/__init__.py
@@ -18,5 +18,5 @@
 """Thoth's prescriptions refresh job."""
 
 __title__ = "thoth-prescriptions-refresh"
-__version__ = "0.5.0"
+__version__ = "0.6.0"
 __author__ = "Fridolin Pokorny <fridolin@redhat.com>"
