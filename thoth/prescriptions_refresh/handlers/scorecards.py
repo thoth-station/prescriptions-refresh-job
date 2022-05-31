8300304d266133ebc86db5b3134066f7b360ace9
mayaCostantini <mcostant@redhat.com>
2022-05-31 11:13:01 +0200

    Add units

--- thoth/prescriptions_refresh/handlers/scorecards.py
+++ thoth/prescriptions_refresh/handlers/scorecards.py
@@ -53,7 +53,9 @@ units:
         package_name: {package_name}
 """
 
-_THOTH_PRESCRIPTIONS_REFRESH_SCORECARD_FRESHNESS = int(os.getenv("THOTH_PRESCRIPTIONS_REFRESH_SCORECARD_FRESHNESS", 2))
+_THOTH_PRESCRIPTIONS_REFRESH_SCORECARD_FRESHNESS_WEEKS = int(
+    os.getenv("THOTH_PRESCRIPTIONS_REFRESH_SCORECARD_FRESHNESS_WEEKS", 2)
+)
 
 
 def _handle_code_review(
@@ -865,7 +867,7 @@ def scorecards(prescriptions: "Prescriptions") -> None:
             f'SELECT * FROM openssf.scorecardcron.scorecard WHERE Repo="github.com/{organization}/{repository} "'
             "AND Date=(SELECT MAX(Date) FROM openssf.scorecardcron.scorecard "
             f'WHERE Repo="github.com/{organization}/{repository}") '
-            f"AND Date > DATE_ADD(CURRENT_DATE(), interval -{_THOTH_PRESCRIPTIONS_REFRESH_SCORECARD_FRESHNESS} week)"
+            f"AND Date > DATE_ADD(CURRENT_DATE(), interval -{_THOTH_PRESCRIPTIONS_REFRESH_SCORECARD_FRESHNESS_WEEKS} week)"
         )
         query_result = query_job.result()
         if query_result.total_rows == 0:
